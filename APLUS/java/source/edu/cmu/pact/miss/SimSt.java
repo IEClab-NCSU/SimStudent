244dc960b910c6d5af99b5184fe8fd8ea72aca72
Raj Shah <rshah28@ncsu.edu>
2021-02-11 12:29:45 -0500

    Adding PR-age files and foil log files inside a user specific directory

--- APLUS/java/source/edu/cmu/pact/miss/SimSt.java
+++ APLUS/java/source/edu/cmu/pact/miss/SimSt.java
@@ -3723,8 +3723,9 @@ public final class SimSt implements Serializable {
 		    		foilData.setFoilLogDir(getProjectDirectory() + WebStartFileDownloader.separator + getFoilLogDir() + WebStartFileDownloader.separator);
 	    		}
 	    		else if(!isWebStartMode()) {
-	    		foilData.setFoilDir();
-	    		foilData.setFoilLogDir(getProjectDir() + WebStartFileDownloader.separator + getFoilLogDir() + WebStartFileDownloader.separator);
+		    		foilData.setFoilDir();
+		    		String foilLogDir = getProjectDir() + WebStartFileDownloader.separator + getLogDirectory() +  WebStartFileDownloader.separator + getUserID() + "-" + getFoilLogDir() +  WebStartFileDownloader.separator;
+		    		foilData.setFoilLogDir(foilLogDir);
 	    		} else {
 	    			foilData.setFoilLogDir(WebStartFileDownloader.SimStWebStartDir+FOIL_LOG + "_" + getUserID() + 
 	    					"_" + FileZipper.formattedDate()+System.getProperty("file.separator"));
@@ -11308,7 +11309,7 @@ public final class SimSt implements Serializable {
        String numR = prettyNumRules();
        String numS = prettyNumAllInstructions();
        ageFileName += "R" + numR + "S" + numS;
-       String fullAgePath = getLogDirectory() + "/" + getPrAgeDir();
+       String fullAgePath = getLogDirectory() + WebStartFileDownloader.separator + getUserID() + "-" + getPrAgeDir();
        
        
        File ageFile = new File( fullAgePath, ageFileName ).getAbsoluteFile();
