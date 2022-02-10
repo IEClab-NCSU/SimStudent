32411ffbc1e91bdeb46139751bf66dd7d3d4d161
tshahri <tshahri@ncsu.edu>
2022-02-10 16:02:00 -0500

    removing lightSIDE folder

--- LightSIDE/BaseAgent/src/basilica2/agents/listeners/plan/StepHandler.java
+++
@@ -1,8 +0,0 @@
-package basilica2.agents.listeners.plan;
-
-import basilica2.agents.components.InputCoordinator;
-
-public interface StepHandler
-{
-	public void execute(Step currentStep, PlanExecutor overmind, InputCoordinator source);
-}
