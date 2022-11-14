204e0a78348ed7e144b2697630d18bedbc4dd8de
subodhgujar <subodhsgujar@gmail.com>
2022-11-14 14:31:05 -0500

    Added shell script to create distribution.

---
+++ Tutors/createDistribution.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+if !command -v "java -version" &>/dev/null; then #just to check if java is installed or not
+  echo "Java is not installed. Please install Java".
+  exit
+fi
+
+if !command -v "ant -version" &>/dev/null; then #just to check ant is installed or not.
+  echo "Ant is not installed. Please install Ant".
+  exit
+fi
+
+name="distribution"
+verbose="N"
+function show_usage() {
+  printf "Usage: ./createDistribution.sh [options [parameters]]\n"
+  printf "\nThis shell script generates distribution for simStudent.\n"
+  printf "Options:\n"
+  printf " -n|--name [name], provide distribution name. Default: distribution\n"
+  printf " -v|--verbose, verbose ant logs printed on terminal\n"
+  printf " -h|--help, prints shell script options\n"
+  exit 0
+}
+case $1 in
+--number | -n)
+  shift
+  name="$1"
+  shift
+  ;;
+--verbose | -v)
+  shift
+  verbose="Y"
+  ;;
+--help | -h)
+  shift
+  show_usage
+  ;;
+*)
+  ;;
+esac
+
+
+echo "Distribution name - $name"
+cd "../APLUS/java/"
+
+echo "Creating ctat.jar..."
+if [[ "$verbose" = "Y" ]]; then
+  ant clean #remove previous ctat.jar
+  ant  #Create new ctat.jar
+else
+  ant clean 1>/dev/null 2>&1
+  ant 1>/dev/null 2>&1
+fi
+
+cd "../../Tutors/Algebra/SimStAlgebraV8"
+echo "Compiling tutors..."
+
+if [[ "$verbose" = "Y" ]]; then
+  command "./compileTutor.sh"
+else
+  command "./compileTutor.sh" 1>/dev/null 2>&1
+fi
+
+cd "../../"
+
+if [ -d "./$name" ]; then
+  echo "\nDistribution name $name already exists and will be deleted to create new distribution.\n"
+  rm -rf "$name"
+fi
+
+mkdir "$name"
+echo "\nCopying content of /Algebra folder to $name/ folder."
+cp -a "./Algebra/." "$name/" #copying content of Algebra to distribution
+
+echo "\nCopying jars"
+rm "./$name/lib/ctat.jar"
+cp "../APLUS/java/lib/ctat.jar" "./$name/lib/"
+cp "../APLUS/java/lib/jsoup-1.15.3.jar" "./$name/lib/"
+cp -a "../SIDE&SLIM jars/." "./$name/lib/"
+
+echo "\nDistribution - '$name' created successfully.\n"
