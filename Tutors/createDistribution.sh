92bc2f16ae8ba0f623af202e5f9ce8029107c967
subodhgujar <subodhsgujar@gmail.com>
2022-11-17 11:27:04 -0500

    Compiling lightside module in automation script to run prediction server

--- Tutors/createDistribution.sh
+++ Tutors/createDistribution.sh
@@ -34,8 +34,8 @@ case $1 in
   shift
   show_usage
   ;;
-*)
-  ;;
+*) ;;
+
 esac
 printf "\nUsing java version: "
 command java -version
@@ -48,7 +48,7 @@ cd "../APLUS/java/"
 printf "\nCreating ctat.jar..."
 if [[ "$verbose" = "Y" ]]; then
   ant clean #remove previous ctat.jar
-  ant  #Create new ctat.jar
+  ant       #Create new ctat.jar
 else
   ant clean 1>/dev/null 2>&1
   ant 1>/dev/null 2>&1
@@ -63,11 +63,11 @@ else
   command "./compileTutor.sh" 1>/dev/null 2>&1
 fi
 
-cd "../../"
+cd "../../Tutors"
 
 if [ -d "./$name" ]; then
   printf "\nDistribution name $name already exists and will be deleted to create new distribution.\n"
-  rm -rf "$name"
+  rm -rf "./$name"
 fi
 
 mkdir "$name"
@@ -81,17 +81,17 @@ cp "../APLUS/java/lib/jsoup-1.15.3.jar" "./$name/lib/"
 cp -a "../SIDE&SLIM jars/." "./$name/lib/"
 
 printf "\nCompiling lightside files..."
+cd "../LightSide/lightside/"
 if [[ "$verbose" = "Y" ]]; then
-  command "ant clean"
-  command "ant"
+  ant clean
+  ant
 else
-  command "ant clean" 1>/dev/null 2>&1
-  command "ant" 1>/dev/null 2>&1
+  ant clean 1>/dev/null 2>&1
+  ant 1>/dev/null 2>&1
 fi
-
+cd "../../Tutors"
 
 printf "\nCopying lightside files..."
 cp -a "../Lightside/lightside" "./$name/"
-
 
 printf "\nDistribution - '$name' created successfully.\n"
